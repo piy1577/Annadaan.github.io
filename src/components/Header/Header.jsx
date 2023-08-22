530aaf8a0290f1bf9d52e4d2beffaab7d3cafb3f
Priyanka Kumari <kumaripriyanka1832@gmail.com>
2023-08-22 14:38:43 +0530

    PRUHK

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Header = () => {
-    return <div>Header</div>;
+    return (
+        <header classname="header">
+            <div className="info">
+                <p>Welcome to our website</p>
+            </div>
+            <div className="contact">
+                <p>Contact us for more information</p>
+                <button type="submit">Submit</button>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
