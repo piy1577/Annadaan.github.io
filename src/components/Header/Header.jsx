94c51a0e79ee7c25aeac8621082173798a091423
Priyanka Kumari <kumaripriyanka1832@gmail.com>
2023-09-09 18:47:47 +0530

    changesheader

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,15 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+//import "./Header.scss"; // Import your SCSS file
+
+//import "./Header.scss";
 
 const Header = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
-        <header classname="header">
-            <div className="info">
-                <p>Welcome to our website</p>
+        <header className={`header ${menuOpen ? "open" : ""}`}>
+            <div className="logo">
+                <a href="#">
+                    <img src="logo.png" alt="logo" />
+                    Annadan
+                </a>
             </div>
-            <div className="contact">
-                <p>Contact us for more information</p>
-                <button type="submit">Submit</button>
+            <div className="menu" onClick={toggleMenu}>
+                <span></span>
+                <span></span>
+                <span></span>
             </div>
+            <ul className={`nav-list ${menuOpen ? "open" : ""}`}>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/Contact">Contact</a>
+                </li>
+                <li>
+                    <a href="/Search">Search</a>
+                </li>
+                <li>
+                    <a href="/Login">Login</a>
+                </li>
+                <li>
+                    <a href="/Signup">Signup</a>
+                </li>
+            </ul>
         </header>
     );
 };
